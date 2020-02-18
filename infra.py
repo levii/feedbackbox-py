2da33f8f1f737b06071776b9679b073b927b995d
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-02-18 21:22:23 +0900

    処理結果をテストしやすく & 見やすくstdoutに出力するために、 Response クラスを実装

--- infra.py
+++ infra.py
@@ -2,21 +2,26 @@ import dataclasses
 import datetime
 import os
 import pickle
-import typing
+
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Union
 
 from models import Feedback
 
 
 @dataclasses.dataclass()
 class Store:
-    feedbacks: typing.List[Feedback] = dataclasses.field(default_factory=list)
+    feedbacks: List[Feedback] = dataclasses.field(default_factory=list)
     created_at: datetime.datetime = dataclasses.field(default_factory=datetime.datetime.utcnow)
 
 
 @dataclasses.dataclass()
 class Repository:
     store: Store = dataclasses.field(default_factory=Store)
-    filename: typing.Optional[str] = None
+    filename: Optional[str] = None
 
     def reset(self):
         self.store = Store()
@@ -39,3 +44,10 @@ class Repository:
 
         with open(self.filename, "wb") as file:
             pickle.dump(self.store, file)
+
+
+@dataclasses.dataclass()
+class Response:
+    mode: str
+    status: int
+    message: Union[List[Any], Dict[Any, Any]]
