758a4bdfd2c68bcbfb9fc245b4551e21f57c94e4
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-02-21 15:59:51 +0900

    テストコードで、コマンド引数を素直に指定するように変更

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,26 +19,28 @@ class TestMain:
     def teardown_method(self, _):
         self.repository.reset()
 
-    def _execute(self, args: typing.List[str]) -> Response:
+    def _execute(self, args: typing.Union[str,typing.List[str]]) -> Response:
+        if isinstance(args, str):
+            args = args.split(" ")
         parser = main.build_parser()
         options = parser.parse_args(args)
         return main.main(mode=options.mode, args=options, injector=self.container)
 
     def test_list_feedbacks(self):
-        response = self._execute(args=["feedback", "list", "--user=1"])
+        response = self._execute("feedback list --user=1")
         assert response.mode == "feedback-list"
         assert response.status == 200
         assert len(response.message) == 0
 
     def test_create_feedback(self):
-        response = self._execute(args="feedback create --title=タイトル --description=本文 --user_id=1".split(" "))
+        response = self._execute("feedback create --title=タイトル --description=本文 --user_id=1")
         assert response.mode == "feedback-create"
         assert response.status == 200
         assert len(response.message) == 1
         feedback = response.message[0]
         assert isinstance(feedback, Feedback)
 
-        list_response = self._execute(args="feedback list --user_id=1".split(" "))
+        list_response = self._execute("feedback list --user_id=1")
         assert list_response.mode == "feedback-list"
         assert list_response.status == 200
         assert len(list_response.message) == 1
