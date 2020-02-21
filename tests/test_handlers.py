479ff86da65d3bd10f1c7956c64e0b7326c4d137
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-02-21 20:59:41 +0900

    FeedbackFetchListHandler のテストを追加

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,6 +1,9 @@
 from handlers import FeedbackCreateHandler
+from handlers import FeedbackFetchListHandler
 from handlers import FeedbackRepository
+from handlers import UserRepository
 from infra import Repository
+from models import User
 from tests.conftest_container import container
 
 
@@ -28,3 +31,53 @@ class TestFeedbackCreateHandler:
         assert len(feedbacks) == 1
         assert feedbacks[0].title == title
         assert feedbacks[0].description == description
+
+
+class TestFeedbackFetchListHandler:
+    customer_user = None
+    customer_other_user = None
+    support_user = None
+
+    customer_user_feedback = None
+    customer_other_user_feedback = None
+
+    @classmethod
+    def setup_class(cls):
+        cls.container = container
+        cls.repository: Repository = cls.container.get(Repository)
+        cls.create_handler: FeedbackCreateHandler = cls.container.get(FeedbackCreateHandler)
+        cls.fetch_handler: FeedbackFetchListHandler = cls.container.get(FeedbackFetchListHandler)
+        cls.user_repository: UserRepository = cls.container.get(UserRepository)
+        cls.feedback_repository: FeedbackRepository = cls.container.get(FeedbackRepository)
+
+    def setup_method(self, _):
+        self.customer_user = User(name="Test Customer User", role="customer")
+        self.user_repository.save(self.customer_user)
+        self.customer_user_feedback = self.create_handler.execute(
+            title="要望タイトル",
+            description="要望の本文",
+            user_id=self.customer_user.user_id,
+        )
+
+        self.customer_other_user = User(name="Test Customer Other User", role="customer")
+        self.user_repository.save(self.customer_other_user)
+        self.customer_other_user_feedback = self.create_handler.execute(
+            title="要望タイトル",
+            description="要望の本文",
+            user_id=self.customer_other_user.user_id,
+        )
+
+        self.support_user = User(name="Support User", role="support")
+        self.user_repository.save(self.support_user)
+
+    def teardown_method(self, _):
+        self.repository.reset()
+
+    def test_fetch_by_customer(self):
+        feedbacks = self.fetch_handler.execute(user_id=self.customer_user.user_id)
+        assert len(feedbacks) == 1
+        assert feedbacks[0] == self.customer_user_feedback
+
+    def test_fetch_by_support(self):
+        feedbacks = self.fetch_handler.execute(user_id=self.support_user.user_id)
+        assert len(feedbacks) == 2
